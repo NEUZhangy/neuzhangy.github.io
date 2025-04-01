6139a469e93927f431e66325fe3efb374e5eeab5
Ying Zhang <yingzhang@vt.edu>
2025-03-31 23:37:47 -0400

    test

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -101,9 +101,10 @@ function App() {
             <p>  I am an Assistant Professor of Computer Science at Wake Forest University (WFU) since 2025. My research interests include Software Engineering and Software Security.
                My research work aims to bridge the gap between theoretical security knowledge and practical implementation. The research mission is to develop comprehensive security frameworks to 
                identify, demonstrate, and mitigate vulnerabilities in open-source software applications, with the goal of equipping developers with enhanced guidance for addressing code vulnerabilities effectively. </p>
-             Before joining WFU,  I worked as a Trust Engineer at LinkedIn from 2023 to 2024. I obtained my Ph.D. degree from Virginia Tech in 2023 under the supervision of <a href="https://people.cs.vt.edu/nm8247/"> Dr. Na Meng </a>  and  <a href="https://people.cs.vt.edu/danfeng/"> Dr. Daphne (Danfeng) Yao </a>. 
+             <p> Before joining WFU,  I worked as a Trust Engineer at LinkedIn from 2023 to 2024. I obtained my Ph.D. degree from Virginia Tech in 2023 under the supervision of <a href="https://people.cs.vt.edu/nm8247/"> Dr. Na Meng </a>  and  <a href="https://people.cs.vt.edu/danfeng/"> Dr. Daphne (Danfeng) Yao </a>. 
                 I received my master's degree from Missouri University of Science and Technology in 2018 and bachelor's degree from Northeastern University (China) in 2016. 
                 During my Ph.D. studies, I spent the summer of 2021 at ByteDance's Application Security team mentored by <a href="https://dingelish.com/"> Dr. Yu Ding </a>  and Dr. Peng Li.</p>
+                </p>
           </div>
 
           <h3 className="text-xl font-bold text-[#000000] mb-3 font-serif">Join the SE Lab</h3>     
